df0149164e7335d338c53f46569743b72a0223f8
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-13 02:56:08 +0000

    Automatically backed up by Learn

---
+++ lib/learning-comments.rb
@@ -0,0 +1,11 @@
+#first comment
+puts (1 #+ 1)
+#Perform a constant expression evaluation
+3
+#Assign constant 3 to bare-world variable trinagle_sides
+'trinagle_sides'
+#From The Three Dog Night song: "Joy to the World(Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
